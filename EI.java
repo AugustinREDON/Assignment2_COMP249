b2c4d4a7b7fa6a951bd46254ac1ddb4f893df11d
Jacob Paterak <jacobpaterak@gmail.com>
2025-03-18 14:56:12 -0400

    Read and wrote from a file and did some Tax things

--- EI.java
+++ EI.java
@@ -1,3 +1,14 @@
 public class EI extends Deductions{
+    public double calculateTax(Employee e) {
+
+        if(e.getGrossSalary()<65700)
+        {
+            return e.getGrossSalary() * 0.0164;
+        }
+        else
+        {
+            return 1077.48;
+        }
+    }
 
 }
