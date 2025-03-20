9cbe97a9af37176ac4106f3ec7af9c33ad4a8206
Augustin Redon <augustin.redon@icloud.com>
2025-03-20 11:08:01 -0400

    fixed Provincial income tax

--- ProvincialIncomeTax.java
+++ ProvincialIncomeTax.java
@@ -7,14 +7,19 @@ public class ProvincialIncomeTax extends Deductions{
     @Override
     public double calculateTax(){
         double taxableIncome = employee.getGrossSalary();
-        double tax = 0.0;
         
-        if(taxableIncome > 129590){
-            tax += (taxableIncome - 129590) * 0.2575;
-            taxableIncome = 129590;
+        if(taxableIncome > 18571 && taxableIncome <= 53255){
+            return taxableIncome * 0.14;
+        } else if (taxableIncome > 53255 && taxableIncome <= 106495){
+            return taxableIncome * 0.19;
+        } else if (taxableIncome > 106495 && taxableIncome <= 129590){
+            return taxableIncome * 0.24;
+        } else if (taxableIncome > 129590 ){
+            return taxableIncome * 0.2575;
+        } else {
+            return 0.0;
         }
         
-        return tax;
     }
 
 }
