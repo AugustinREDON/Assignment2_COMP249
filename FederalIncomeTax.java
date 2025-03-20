a87240eab3ac9261bd5f8851f73bd0858d6fcb58
Augustin Redon <augustin.redon@icloud.com>
2025-03-20 10:59:38 -0400

    Fixed federal Income tax

--- FederalIncomeTax.java
+++ FederalIncomeTax.java
@@ -5,22 +5,22 @@ public class FederalIncomeTax extends Deductions{
             //TODO Auto-generated constructor stub
         }
 
-        private double grosssalary;
-
     public double calculateTax() {
 
-        if(employee.getGrossSalary() > 16129)
+        double taxableIncome = employee.getGrossSalary();
+
+        if(taxableIncome > 16129)
             return 0;
-        else if(employee.getGrossSalary() >= 16129 && employee.getGrossSalary() <= 57375)
-            return employee.getGrossSalary() * 0.15;
-        else if(this.grosssalary > 57375 && this.grosssalary <= 114750) // should these be employee.getGross salary instead of this.grossSalary?
-            return this.grosssalary * 0.205;
-        else if(this.grosssalary > 113751 && this.grosssalary <= 177882)
-            return this.grosssalary * 0.26;
-        else if(this.grosssalary > 177883 && this.grosssalary <= 253414)
-            return this.grosssalary * 0.29;
+        else if(taxableIncome >= 16129 && taxableIncome <= 57375)
+            return taxableIncome * 0.15;
+        else if(taxableIncome > 57375 && taxableIncome <= 114750) // should these be employee.getGross salary instead of taxableIncome?
+            return taxableIncome * 0.205;
+        else if(taxableIncome > 113751 && taxableIncome <= 177882)
+            return taxableIncome * 0.26;
+        else if(taxableIncome > 177883 && taxableIncome <= 253414)
+            return taxableIncome * 0.29;
         else
-            return this.grosssalary * 0.33;
+            return taxableIncome * 0.33;
     }
 
 
