167d5f368e1d5f72200c58650e354b64589b626e
Augustin Redon <augustin.redon@icloud.com>
2025-03-21 11:42:27 -0400

    comments

--- Employee.java
+++ Employee.java
@@ -1,42 +1,50 @@
-public class Employee{
+//--------------------------------------
+// Assignment 2
+// Written by: Augustin Redon 40240986 - 
+//--------------------------------------
 
-    private long EmployeeId;
-    private String FirstName;
-    private String LastName;
-    private double HourlyWage;
-    private double HoursWorked;
-    private double GrossSalary;
-    final int week = 52;
+// Employee class to represent an employee's details and calculate their gross salary
+public class Employee {
 
-    public Employee(Long id, String firstName, String lastName,double HourlyWage, double HoursWorked) {
-        this.EmployeeId = id;
-        this.FirstName = firstName;
-        this.LastName = lastName;
-        this.HourlyWage = HourlyWage;
-        this.HoursWorked = HoursWorked;
-        this.GrossSalary = HourlyWage * HoursWorked * week;
+    // Fields to store employee details
+    private long EmployeeId;       
+    private String FirstName;      
+    private String LastName;       
+    private double HourlyWage;     
+    private double HoursWorked;    
+    private double GrossSalary;    
+    final int week = 52;           
 
-    }
-    public double getGrossSalary(){
-        return this.GrossSalary;
-    }
-
-    public double getEmployeeId(){
-        return EmployeeId;
-    }
-
-    public String getFirstName(){
-        return FirstName;
+    // Constructor to initialize an Employee object
+    public Employee(Long id, String firstName, String lastName, double HourlyWage, double HoursWorked) {
+        this.EmployeeId = id;                      
+        this.FirstName = firstName;                
+        this.LastName = lastName;                  
+        this.HourlyWage = HourlyWage;              
+        this.HoursWorked = HoursWorked;            
+        this.GrossSalary = HourlyWage * HoursWorked * week;  
     }
 
-    public String getLastName(){
-        return LastName;
+    // Getter methods to retrieve employee information
+    public double getGrossSalary() {
+        return this.GrossSalary; 
     }
 
-    public String toString(){
+    public double getEmployeeId() {
+        return EmployeeId; 
+    }
+
+    public String getFirstName() {
+        return FirstName; 
+    }
+
+    public String getLastName() {
+        return LastName; 
+    }
+
+    // Override the toString() method to provide a string representation of the employee
+    @Override
+    public String toString() {
         return EmployeeId + "\t" + FirstName + "\t" + LastName + "\t" + GrossSalary;
     }
-
-
-
 }
